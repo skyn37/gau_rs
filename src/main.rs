05251ea9dbf2ebcd2263eafa79efd07ee58b8773
ipwnucry <tsvetomir.kyukov@ft.com>
2025-03-10 20:12:22 +0200

    basic http call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip").await?;
+    let body = resp.text().await?;
+    let json = serde_json::from_str::<HashMap<String, String>>(&body)?;
+    println!("{json:#?}");
+    Ok(())
 }
